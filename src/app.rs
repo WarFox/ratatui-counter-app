a8ad3f9e2b22e9903bc3a1f212e5705be3e1cd0a
Deepu Mohan Puthrote <git@deepumohan.com>
2024-01-27 17:37:32 +0000

    add implementation for App

--- src/app.rs
+++ src/app.rs
@@ -1,5 +1,52 @@
 // App State
+#[derive(Default)]
 pub struct App {
     pub counter: u8,
     pub should_quit: bool,
+}
+
+impl App {
+    /// Constructs a new instance of [`App`].
+    pub fn new() -> Self {
+        Self::default()
+    }
+
+    /// Handles the tick event of the terminal.
+    pub fn tick(&self) {}
+
+    /// Set should_quit to true to quit the application.
+    pub fn quit(&mut self) {
+        self.should_quit = true;
+    }
+
+    pub fn increment_counter(&mut self) {
+        self.counter = self.counter + 1;
+    }
+
+    pub fn decrement_counter(&mut self) {
+        if let Some(res) = self.counter.checked_sub(1) {
+            self.counter = res;
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[test]
+    fn test_app_increment_counter() {
+        let mut app = App::default();
+        app.increment_counter();
+        assert_eq!(app.counter, 1);
+
+        app.increment_counter();
+        assert_eq!(app.counter, 2);
+    }
+
+    #[test]
+    fn test_app_decrement_counter() {
+        let mut app = App::default();
+        app.decrement_counter();
+        assert_eq!(app.counter, 0);
+    }
 }
